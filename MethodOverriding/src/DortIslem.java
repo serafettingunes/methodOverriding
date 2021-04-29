9040dea48cd5aa15a20d42390127c64af9f99852
serafettingunes <54938342+attila40x@users.noreply.github.com>
2021-04-30 01:19:26 +0300

    Method overiding

---
+++ MethodOverriding/src/DortIslem.java
@@ -0,0 +1,11 @@
+
+public class DortIslem {
+	public int topla(int a,int b)
+	{
+		return a+b;
+	}
+	public int  topla(int x,int y, int z)
+	{
+		return x+y+z;
+	}
+}
---
+++ MethodOverriding/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem d=new DortIslem();
+	System.out.println(d.topla(5, 5,6));
+		
+
+	}
+
+}
